eb9e6d9a88705939938d3b4520435e39e8792fc7
LTU´s Computer <asolanas93@gmail.com>
2016-12-05 16:56:33 +0100

    new xbox controller

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var Cylon = require("cylon");
 var ffmpeg = require("ffmpeg");
+var xbox = require("xbox-controller-node");
 
 Cylon.api("http",{
     port: 8080,
@@ -15,58 +16,26 @@ Cylon.robot({
 
     connections: {
         ardrone: { adaptor: 'ardrone', port: "192.168.1.1"},
-        joystick:{ adaptor: 'joystick'}
-
+        leapmotion: { adaptor: 'leapmotion'}
     },
 
     devices: {
         drone: {driver: 'ardrone'},
-        controller: {driver: 'xbox-360'}
+        leapmotion: { driver: 'leapmotion'}
     },
 
     work: function(my) {
-        ["a", "b", "x", "y"].forEach(function(button) {
-            my.controller.on(button + ":press", function() {
-                console.log("Button " + button + " pressed.");
-            });
-
-            my.controller.on(button + ":release", function() {
-                if (landed){
-                    my.drone.takeoff();
-                    landed = false
-                }else{
-                    my.drone.land();
-                    landed = true
-                }
-                console.log("Button " + button + " released.");
-            });
-        });
-
-        my.controller.on("left_x:move", function(pos) {
-            console.log("Left Stick - X:", pos);
-        });
-
-        my.controller.on("left_y:move", function(pos) {
-            console.log("Left Stick - Y:", pos);
-            my.drone.up(0.1);
-
-        });
-
-        my.controller.on("right_x:move", function(pos) {
-            console.log("Right Stick - X:", pos);
-
-        });
-
-        my.controller.on("right_y:move", function(pos) {
-            console.log("Right Stick - Y:", pos);
-        });
-
-        my.controller.on("lt:move", function(pos) {
-            console.log("Left Trigger: ", pos);
-        });
-
-        my.controller.on("rt:move", function(pos) {
-            console.log("Right Trigger: ", pos);
+        ["a","b","x","y"].forEach(function(button){
+           xbox.on(button, function(){
+               console.log('['+button+'] button press');
+               if(landed){
+                   my.drone.takeoff();
+                   landed = false;
+               }else{
+                   my.drone.land();
+                   landed = true;
+               }
+           });
         });
     }
 });
