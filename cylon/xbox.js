4b403580420cd8ee025fe6ad15c1c28dcbd0e3bf
Alejandro Solanas <asolanas93@gmail.com>
2016-12-05 14:09:47 +0100

    xbox controller

--- cylon/xbox.js
+++ cylon/xbox.js
@@ -1,27 +1,12 @@
-"use strict";
-
-var Cylon = require("cylon");
-var ffmpeg = require("ffmpeg");
-
-Cylon.api("http",{
-    port: 8080,
-    ssl: false
-});
-
-var landed = true;
+var Cylon = require('cylon');
 
 Cylon.robot({
-    name: "LeaDrone",
-
     connections: {
-        ardrone: { adaptor: 'ardrone', port: "192.168.1.1"},
-        joystick:{ adaptor: 'joystick'}
-
+        joystick: { adaptor: 'joystick' }
     },
 
     devices: {
-        drone: {driver: 'ardrone'},
-        controller: {driver: 'xbox-360'}
+        controller: { driver: 'xbox-360' }
     },
 
     work: function(my) {
@@ -31,13 +16,6 @@ Cylon.robot({
             });
 
             my.controller.on(button + ":release", function() {
-                if (landed){
-                    my.drone.takeoff();
-                    landed = false
-                }else{
-                    my.drone.land();
-                    landed = true
-                }
                 console.log("Button " + button + " released.");
             });
         });
@@ -48,13 +26,10 @@ Cylon.robot({
 
         my.controller.on("left_y:move", function(pos) {
             console.log("Left Stick - Y:", pos);
-            my.drone.up(0.1);
-
         });
 
         my.controller.on("right_x:move", function(pos) {
             console.log("Right Stick - X:", pos);
-
         });
 
         my.controller.on("right_y:move", function(pos) {
