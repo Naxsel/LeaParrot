5f52d9940bab9c3036aad728d68ab82b1f53d252
LTU´s Computer <asolanas93@gmail.com>
2016-12-06 13:24:12 +0100

    drone commands for xbox

--- app.js
+++ app.js
@@ -27,32 +27,53 @@ Cylon.robot({
     },
 
     work: function(my) {
+
+        my.drone.takeoff();
+
+        //Leap Motion Mapping
+
+        //Xbox Controller Mapping
         xbox.on('leftstickDown', function () {
             console.log('Moving [LEFTSTICK] DOWN');
+            my.drone.back(0.3);
         });
-
         xbox.on('leftstickUp', function () {
             console.log('Moving [LEFTSTICK] UP');
+            my.drone.front(0.3);
         });
-
         xbox.on('rightstickLeft', function () {
             console.log('Moving [RIGHTSTICK] LEFT');
+            my.drone.counterClockwise(0.2);
         });
         xbox.on('rightstickRight', function () {
             console.log('Moving [RIGHTSTICK] RIGHT');
+            my.drone.clockwise(0.2);
         });
 
         xbox.on('rightstickDown', function () {
             console.log('Moving [RIGHTSTICK] DOWN');
+            if (my.drone.fly)
+            my.drone.down(0.3)
         });
         xbox.on('rightstickUp', function () {
             console.log('Moving [RIGHTSTICK] UP');
+            my.drone.up(0.3)
         });
         xbox.on('leftstickRight', function () {
             console.log('Moving [LEFTSTICK] RIGHT');
+            my.drone.right(0.3);
         });
         xbox.on('leftstickLeft', function () {
             console.log('Moving [LEFTSTICK] LEFT');
+            my.drone.left(0.3);
+        });
+
+
+        after((10).seconds(), function(){
+            my.drone.land();
+        });
+        after((15).seconds(), function(){
+            my.drone.land();
         });
     }
 });
