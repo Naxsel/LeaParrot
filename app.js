aaa52dac85bc4d2e7505605ee48e8079bf675a36
Alejandro Solanas <asolanas93@gmail.com>
2016-12-07 19:33:26 +0100

    leap motion controls added

--- app.js
+++ app.js
@@ -38,6 +38,68 @@ Cylon.robot({
 
         //Leap Motion Mapping
 
+        my.leapmotion.on('frame', function(frame){
+            if(frame.hands.length > 0){
+                my.drone.takeoff();
+            } else {
+                my.drone.land();
+            }
+
+            if(frame.valid && frame.gestures.length > 0){
+                frame.gestures.forEach(function(g){
+                    if(g.type == 'swipe'){
+                        var currentPosition = g.position;
+                        var startPosition = g.startPosition;
+
+                        var xDirection = currentPosition[0] - startPosition[0];
+                        var yDirection = currentPosition[1] - startPosition[1];
+                        var zDirection = currentPosition[2] - startPosition[2];
+
+                        var xAxis = Math.abs(xDirection);
+                        var yAxis = Math.abs(yDirection);
+                        var zAxis = Math.abs(zDirection);
+
+                        var superiorPosition  = Math.max(xAxis, yAxis, zAxis);
+
+                        if(superiorPosition === xAxis){
+                            if(xDirection < 0){
+                                console.log('LEFT');
+                                my.drone.left();
+                            } else {
+                                my.drone.right();
+                                console.log('RIGHT');
+                            }
+                        }
+
+                        if(superiorPosition === zAxis){
+                            if(zDirection > 0){
+                                console.log('BACKWARDS');
+                                my.drone.back();
+                            } else {
+                                console.log('FORWARD');
+                                my.drone.forward();
+                            }
+                        }
+
+                        if(superiorPosition === yAxis){
+                            if(yDirection > 0){
+                                console.log('UP');
+                                my.drone.up(1);
+                            } else {
+                                console.log('DOWN');
+                                my.drone.down(1);
+                            }
+                        }
+                    } else if(g.type === 'keyTap'){
+                        my.drone.backFlip();
+                        after((5).seconds(), function(){
+                            my.drone.land();
+                        })
+                    }
+                })
+            }
+        });
+
         //Xbox Controller Mapping
         xbox.on('leftstickDown', function () {
             console.log('Moving [LEFTSTICK] DOWN');
